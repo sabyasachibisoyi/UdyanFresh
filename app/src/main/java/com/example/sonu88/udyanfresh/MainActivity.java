6b39d3201c353453c3281329ac15078aa82dbe1c
sabyasachibisoyi <sabyasachibisoyi111@gmail.com>
2016-08-11 20:29:36 +0530

    sonu

---
+++ app/src/main/java/com/example/sonu88/udyanfresh/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sonu88.udyanfresh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
